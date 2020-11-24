bc8fb26744b0d750c82d29303b5c742e24d66bcf
marshacw <mcwibowo@g.harvard.edu>
2020-11-23 21:12:09 -0500

    Update quality_control.sh

--- read_processing_and_quality_control/quality_control.sh
+++ read_processing_and_quality_control/quality_control.sh
@@ -2,14 +2,35 @@
 
 #run this script per sample
 
-sample_name=${1} #e.g.AZ107
+sample_name=$1 #e.g.AZ107
 
 #run AdapterRemoval
-AdapterRemoval --file1 "${sample_name}_1.fq" --file2 "${sample_name}_2.fq" --basename "${sample_name}" --adapter1 AGATCGGAAGAGCACACGTCTGAACTCCAGTCACNNNNNNNATCTCGTATGCCGTCTTCTGCTTG --adapter2 AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTNNNNNNNGTGTAGATCTCGGTGGTCGCCGTATCATT --trimns --trimqualities --threads 15
+AdapterRemoval \
+	--file1 "${sample_name}_1.fq" \
+	--file2 "${sample_name}_2.fq" \
+	--basename "${sample_name}" \
+	--adapter1 AGATCGGAAGAGCACACGTCTGAACTCCAGTCACNNNNNNNATCTCGTATGCCGTCTTCTGCTTG \
+	--adapter2 AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTNNNNNNNGTGTAGATCTCGGTGGTCGCCGTATCATT \
+	--trimns \
+	--trimqualities \
+	--threads 15
 
 #run KneadData
-kneaddata --input "${sample_name}_1.fq" --input /home/ubuntu/"${sample_name}_2.fq" --reference-db /mnt/pkgs/knead_database/ --threads 30 --output "${sample_name}" --bypass-trim
+kneaddata \
+	--input "${sample_name}.pair1.truncated" \
+	--input /home/ubuntu/"${sample_name}.pair2.truncated" \
+	--reference-db /mnt/pkgs/knead_database/ \
+	--threads 30 \
+	--output "${sample_name}" \
+	--bypass-trim
 
 #run Cutadapt
-cutadapt --cores=35 --minimum-length 30 -o "${sample_name}_filtered_pair1.fq" -p "${sample_name}_filtered_pair2.fq" "${sample_name}_kneaddata_paired_1.fastq" "${sample_name}_kneaddata_paired_2.fastq" &>"${sample_name}_out_messages.txt"
+cutadapt \
+	--cores=35 \
+	--minimum-length 30 \
+	-o "${sample_name}_filtered_pair1.fq" \
+	-p "${sample_name}_filtered_pair2.fq" \
+	"${sample_name}_kneaddata_paired_1.fastq" "${sample_name}_kneaddata_paired_2.fastq" &>"${sample_name}_out_messages.txt"
+
+
 
